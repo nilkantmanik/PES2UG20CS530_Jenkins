fa367eccf22951505664952dcc4d9ceadbcc1ccf
nilkantmanik <118300824+nilkantmanik@users.noreply.github.com>
2023-02-17 19:22:16 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout<<"Nilkant,PES2UG20CS530\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
