8abf75f76f41de6d988b8e4a51d1a495018b6ae8
nilkantmanik <118300824+nilkantmanik@users.noreply.github.com>
2023-02-17 20:03:37 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout<<"Nilkant,PES2UG20CS530\n";
+  cout<<"NILKANT\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
